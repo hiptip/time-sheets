8969975463074ff5ddcae8e109d794645e737640
hiptip <bicknoston@gmail.com>
2024-05-04 22:03:37 -0400

    testing

--- sendEmail.js
+++ sendEmail.js
@@ -12,8 +12,8 @@ const sendPDF = (subject) => {
 
     const mailOptions = {
       from: 'northcarolinaroadbusiness@gmail.com',
-      to: ['justinwills02@hotmail.com', 'jonesk@k2dwconsulting.com', 'lindawilliams101254@gmail.com', 'rocbraz@gmail.com'],
-      // to: ['bicknoston@gmail.com'],
+      // to: ['jonesk@k2dwconsulting.com', 'lindawilliams101254@gmail.com', 'rocbraz@gmail.com'],
+      to: ['bicknoston@gmail.com'],
       subject: subject,
       text: 'Please find attached the time sheet for the day.',
       attachments: [
