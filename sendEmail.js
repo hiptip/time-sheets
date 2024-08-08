64af5d331f876b010d2fc72dbbf515b40f2ac119
hiptip <bicknoston@gmail.com>
2024-08-07 22:35:55 -0400

    remove email

--- sendEmail.js
+++ sendEmail.js
@@ -12,7 +12,7 @@ const sendPDF = (subject) => {
 
     const mailOptions = {
       from: 'northcarolinaroadbusiness@gmail.com',
-      to: ['jonesk@k2dwconsulting.com', 'lwilliamstssam@gmail.com', 'rocbraz@gmail.com'],
+      to: ['jonesk@k2dwconsulting.com', 'lwilliamstssam@gmail.com'],
       subject: subject,
       text: 'Please find attached the time sheet for the day.',
       attachments: [
